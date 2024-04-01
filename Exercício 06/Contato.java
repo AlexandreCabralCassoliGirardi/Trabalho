23bd72022930b5060863af0d8d811029c6dfa78b
AlexandreCabralCassoliGirardi <144747962+AlexandreCabralCassoliGirardi@users.noreply.github.com>
2024-03-31 23:52:59 -0300

    Create Contato.java

---
+++ Exercício 06/Contato.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Contato {
+    private String nome;
+    private String telefone;
+
+    public Contato(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+}
+
+class Agenda {
+    private List<Contato> contatos;
+
+    public Agenda() {
+        this.contatos = new ArrayList<>();
+    }
+
+    public void adicionarContato(Contato contato) {
+        contatos.add(contato);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Contato adicionado: " + contato.getNome());
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
+    public Contato buscarContatoPorNome(String nome) {
+        for (Contato contato : contatos) {
+            if (contato.getNome().equalsIgnoreCase(nome)) {
+                return contato;
+            }
+        }
+        return null;
+    }
+}
